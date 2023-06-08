e12bd6d9e5da49fec0c5d439c148044ab4653e7b
Sylvia Llorente <sylviall81@gmail.com>
2023-06-08 09:32:10 +0200

    funciones favorito y next fact en produccion

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,14 @@
 
+const arrayFavFacts = [];
 
 let factPlaceHolder = document.getElementById("selected-useless-fact");
 
-fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
+let getUselessFact = fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
   .then(response => response.json())
   .then(data => factPlaceHolder.innerHTML = data.text);
   
   //.then(data => console.log(data.text));
+
 
 
   function makeSound(id) {
@@ -17,4 +19,19 @@ function pauseSound(id){
   let audio = document.getElementById(id);
   audio.pause();
 }
+
+function nextFact(){
+  return getUselessFact;
+};
+
+function getMyFavorites(){
+  let favoriteFact = document.getElementById('selected-useless-fact').innerText
+  console.log(favoriteFact);
+  arrayFavFacts.push(favoriteFact); 
+  console.log (arrayFavFacts);
+}
+
+
+
+
 
