8992039d8fba81779ed1fa837e463b5fa9e46bf0
Sylvia Llorente <sylviall81@gmail.com>
2023-06-08 12:38:00 +0200

    funcion guardar favoritos

--- js/functions.js
+++ js/functions.js
@@ -1,37 +1,60 @@
+getUselessFact()
+
+function makeSound(id) {
+  let audio = document.getElementById(id);
+  audio.play();
+}
+
+function pauseSound(id) {
+  let audio = document.getElementById(id);
+  audio.pause();
+}
 
 const arrayFavFacts = [];
 
 let factPlaceHolder = document.getElementById("selected-useless-fact");
 
-let getUselessFact = fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
-  .then(response => response.json())
-  .then(data => factPlaceHolder.innerHTML = data.text);
-  
+
+function getUselessFact() {
+  fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
+    .then(response => response.json())
+    .then(data => factPlaceHolder.innerHTML = data.text);
   //.then(data => console.log(data.text));
-
-
-
-  function makeSound(id) {
-  let audio = document.getElementById(id);
-  audio.play();
-}
-function pauseSound(id){
-  let audio = document.getElementById(id);
-  audio.pause();
 }
 
-function nextFact(){
-  return getUselessFact;
+
+function nextFact() {
+  getUselessFact()
 };
 
-function getMyFavorites(){
+function getMyFavorites() {
   let favoriteFact = document.getElementById('selected-useless-fact').innerText
   console.log(favoriteFact);
-  arrayFavFacts.push(favoriteFact); 
-  console.log (arrayFavFacts);
+  arrayFavFacts.push(favoriteFact);
+  console.log(arrayFavFacts);
+  printFavorites(arrayFavFacts);
+
+  function printFavorites(array) {
+
+    /*const scrollableContainer = document.getElementById('scrollable-container');*/
+    const favoritesList = document.getElementsByClassName("favorites-list");
+    //const favoritesItems = document.getElementsByClassName("list-items");
+
+    for (let i = 0; i < array.length; i++) {
+      let index = i;
+  
+      let listItem = document.createElement('li');
+      listItem.className = 'list-items';
+      listItem.innerText = array[index];
+      //listItem.textContent = array[index];
+
+      listItem.appendChild(favoritesList);
+    }
+
+
+
+
+  }
+
+
 }
-
-
-
-
-
