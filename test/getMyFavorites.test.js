ea3e5caa92773bade4a314a2556953694d06ba94
Sylvia Llorente <sylviall81@gmail.com>
2023-06-12 22:51:20 +0200

    nuevos intentos de testing con helpers -todos fallidos-

--- test/getMyFavorites.test.js
+++ test/getMyFavorites.test.js
@@ -1,5 +1,31 @@
 
 const { getMyFavorites } = require('../js/functions');
+const { load } = require ("../helpers/helpers");
+import { JSDOM } from "jsdom"
+
+
+
+// Ejemplo de prueba
+test('agrega un elemento a arrayFavFacts y llama a la función printFavorite', async () => {
+    const dom = await JSDOM.fromFile("index.html");
+    global.document = document;
+    
+ 
+  
+  let { document } = await load("index.html");
+  
+  // Crea una implementación simulada de la función printFavorite
+  const printFavoriteMock = jest.fn();
+  global.printFavorite = printFavoriteMock;
+
+  // Llama a la función que quieres probar
+  getMyFavorites();
+
+  // Realiza las comprobaciones o aserciones
+  expect(printFavoriteMock).toHaveBeenCalledTimes(1);
+  expect(printFavoriteMock).toHaveBeenCalledWith('Un hecho inútil');
+});
+
 
 
 
