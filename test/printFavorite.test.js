6d51684dd26d5e50f9c1b773f497d8e3a6bbeef5
Sylvia Llorente <sylviall81@gmail.com>
2023-06-13 12:40:30 +0200

    sugerencias testing alex

--- test/printFavorite.test.js
+++ test/printFavorite.test.js
@@ -1,20 +1,20 @@
 
 const { load } = require ("../helpers/helpers");
 const { printFavorite } = require('../js/functions');
+const {}
 
 
 // Ejemplo de prueba
 test('agrega un elemento a la lista de favoritos', async () => {
   
-  let { document } = await load("index.html");
-  
-  //global.document = dom.window.document;
+  let { document } = await load("index.html");  
+  global.document = document
 
   // Llama a la función que quieres probar
   printFavorite('Ejemplo de favorito');
 
   // Realiza las comprobaciones o aserciones
-  const favoritesList = document.getElementsByTagName('ul')[0];
+  const favoritesList = global.document.getElementsByTagName('ul')[0];
   const listItem = favoritesList.querySelector('.list-items');
   expect(listItem).not.toBeNull();
   expect(listItem.textContent).toBe('Ejemplo de favorito');
