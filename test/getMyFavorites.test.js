7f4b953509d005987d18e7d03c4e7aea4db39893
Sylvia Llorente <sylviall81@gmail.com>
2023-06-15 17:46:34 +0200

    eliminar test antiguos

---
+++ test/getMyFavorites.test.js
@@ -0,0 +1,26 @@
+const { getMyFavorites } = require('../js/functions');
+const { load } = require ("../helpers/helpers");
+
+
+
+
+// Ejemplo de prueba
+test('agrega un elemento a arrayFavFacts y llama a la función printFavorite', async () => {
+    const dom = await JSDOM.fromFile("index.html");
+    global.document = document;
+    
+ 
+  
+  let { document } = await load("index.html");
+  
+  // Crea una implementación simulada de la función printFavorite
+  const printFavoriteMock = jest.fn();
+  global.printFavorite = printFavoriteMock;
+
+  // Llama a la función que quieres probar
+  getMyFavorites();
+
+  // Realiza las comprobaciones o aserciones
+  expect(printFavoriteMock).toHaveBeenCalledTimes(1);
+  expect(printFavoriteMock).toHaveBeenCalledWith('Un hecho inútil');
+});
