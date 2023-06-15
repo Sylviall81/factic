ae675cbfa1187dd390ffce33b5fe0bf0551a0c92
Sylvia Llorente <sylviall81@gmail.com>
2023-06-15 17:41:56 +0200

    traspaso test rama testTrials

---
+++ test/getUselessFact.test.js
@@ -0,0 +1,21 @@
+const { load } = require ("../helpers/helpers");
+
+const fetchMock = require('jest-fetch-mock');
+const { getUselessFact } = require('../js/functions');
+
+
+// Configura el mock de fetch
+fetchMock.enableMocks();
+
+// Ejemplo de prueba
+test('obtiene un hecho inútil y lo asigna al factPlaceHolder', async () => {
+
+    let { document } = await load("index.html");
+  
+  // Llama a la función que quieres probar
+  await getUselessFact();
+
+  // Realiza las comprobaciones o aserciones
+  expect(fetchMock).toHaveBeenCalledWith('https://uselessfacts.jsph.pl/api/v2/facts/random');
+  expect(document.getElementById('selected-useless-fact').innerHTML).not.toBe('');
+});
